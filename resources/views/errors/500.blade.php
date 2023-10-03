232434596e87759e3e0524cc86674d1c1af33533
glissattitude <120647351+glissattitude@users.noreply.github.com>
2023-10-03 14:45:30 +0200

    built css

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,35 @@
+@section('title', __('Server Error'))
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight" style="position:relative">
+            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><img src="{{ asset('storage/omedis.png') }}" /></a></li>
+                    <li class="breadcrumb-item">Error - {{ __('Server Error') }}</li>
+                </ol>
+                <p id="version">VERSION<br/><span class="value">{{ \App\Models\History::getLastVersion() }}</span></p>
+            </nav>
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+                <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+                    <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+                        <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                            500
+                        </div>
+
+                        <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                            {{__($exception->getMessage() ?: 'Server Error')}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</x-app-layout>
