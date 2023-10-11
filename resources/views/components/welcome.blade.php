7940f8bfddc4179e2014470ded4aed70545a5f63
glissattitude <120647351+glissattitude@users.noreply.github.com>
2023-10-11 08:34:35 +0200

    doc

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -70,7 +70,7 @@
                 <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
             </svg>
             <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://laracasts.com">Tools</a>
+                <a href="#">Tools</a>
             </h2>
         </div>
 
@@ -81,11 +81,6 @@
                 <li><a href="{{ route('variant.uploadcsv') }}">Omedis csv file checker and validator</a></li>
                 <li><a href="{{ route('variant.uploadxml') }}">Omedis xml file checker and validator</a></li>
             </ul>
-
-
-
-
-
         </p>
 
     </div>
@@ -96,7 +91,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
             </svg>
             <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://tailwindcss.com/">Contributing</a>
+                <a href="#">Contributing</a>
             </h2>
         </div>
 
@@ -111,7 +106,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
             </svg>
             <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://laravel.com/docs">Authentification</a>
+                <a href="#">Authentification</a>
             </h2>
         </div>
 
