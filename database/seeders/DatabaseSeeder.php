bad4ad18b191eabdd37d413d3987c10190ba368a
glissattitude <120647351+glissattitude@users.noreply.github.com>
2024-09-09 17:21:04 +0200

    User seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,13 +9,18 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        \DB::table('users')->delete();
+        \DB::table('users')->insert(array (
+            0 =>
+                array (
+                    'id' => 1,
+                    'name' => 'Administrateur général',
+                    'email' => 'eric.collard@free.fr',
+                    'password' => '$2y$10$Qy.UROfMdVwkZKQlopVjWOapcHwDxLeLIG.4z7E.AJ7C0x2nv7b8e',
+                ),
+        ));
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
